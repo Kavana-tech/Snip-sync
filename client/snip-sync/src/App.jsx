e89cc418687d02fe1907f7f18f5d313fd4e870c2
Kavana-tech <kavanar07@gmail.com>
2025-05-01 16:04:22 +0530

    adding profile features

--- client/snip-sync/src/App.jsx
+++ client/snip-sync/src/App.jsx
@@ -5,6 +5,8 @@ import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
 import AddTeam from "./pages/AddTeam";
+import Profile from "./pages/profile";
+
 function App() {
   return (
     <BrowserRouter>
@@ -14,6 +16,8 @@ function App() {
         <Route path="/signup" element={<SignUp />}></Route>
         <Route path="/dashboard" element={<Dashboard />}></Route>
         <Route path="/addteam" element={<AddTeam />}></Route>
+        <Route path="/profile"  element={<Profile/>}></Route>
+       
       </Routes>
     </BrowserRouter>
   );
